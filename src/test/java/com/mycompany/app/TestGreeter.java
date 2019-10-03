a62683f8bda593f24b5b5c35db5a2c404ee3a0ee
kuku_2612 <kuku_2612@kunjals-air.wireless.concordia.ca>
2019-10-03 17:42:27 -0400

    java file changed successfully

--- src/test/java/com/mycompany/app/TestGreeter.java
+++ src/test/java/com/mycompany/app/TestGreeter.java
@@ -1,7 +1,8 @@
-package com.company.app;
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class TestGreeter {
 	
